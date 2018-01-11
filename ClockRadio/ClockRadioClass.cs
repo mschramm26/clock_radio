1c0d4218e416cba3844baadb6cdd39e68d52d196
mschramm26 <melissa.schramm@gmail.com>
2018-01-11 10:36:57 -0600

    First commit

---
+++ ClockRadio/ClockRadioClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class ClockRadioClass
+    {
+        private string currentTime = "10:30 AM";
+        private string currentAlarmState = "OFF";
+        private string currentAlarmTime = "12:00 PM";
+        private string currentRadioStation = "99.1 FM";
+
+        public void alarmClockFunction()
+        {
+            Console.WriteLine("Welcome to the alarm clock!");
+            Console.WriteLine("The current time is: " + currentTime + "\nThe alarm is currently: " + currentAlarmState + "\nThe alarm is currently set to: " + currentAlarmTime + "\nThe current radio station is: " + currentRadioStation + "\n\nTo change the time, enter T.\nTo change the current state of the alarm, enter S.\nTo change the alarm time, press A.\nTo change the radio station, enter R.\nTo exit the program, enter E.\n");
+            string userInputToChangeSomething = Console.ReadLine();
+
+            bool booleanForWhileLoop = true;
+            while (booleanForWhileLoop)
+            {
+                switch (userInputToChangeSomething)
+                {
+                    case "T":
+                        ChangeTime();
+                        break;
+                    case "S":
+                        Console.WriteLine("The current state of the alarm is " + currentAlarmState + ". Please enter a new state of the alarm.");
+                        currentAlarmState = Console.ReadLine();
+                        break;
+                    case "A":
+                        Console.WriteLine("The alarm is currently set to " + currentAlarmTime + ". Please enter a new alarm time.");
+                        currentAlarmTime = Console.ReadLine();
+                        break;
+                    case "R":
+                        Console.WriteLine("The radio is currently set to " + currentRadioStation + ". Please enter a new radio station.");
+                        currentRadioStation = Console.ReadLine();
+                        break;
+                    case "E":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter valid input.\n");
+                        break;
+                }
+                Console.WriteLine("The current time is: " + currentTime + "\nThe alarm is currently: " + currentAlarmState + "\nThe alarm is currently set to: " + currentAlarmTime + "\nThe current radio station is: " + currentRadioStation + "\n\nTo change the time, enter T.\nTo change the current state of the alarm, enter S.\nTo change the alarm time, enter A.\nTo change the radio station, enter R.\nTo exit the program, enter E.\n");
+                userInputToChangeSomething = Console.ReadLine();
+            }
+            Console.ReadLine();
+            return;
+        }
+        private void ChangeTime()
+        {
+            Console.WriteLine("The current time is " + currentTime + ". Please enter a new time.");
+            currentTime = Console.ReadLine();
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ClockRadio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockRadioClass clockRadioProgram = new ClockRadioClass();
+            clockRadioProgram.alarmClockFunction();
+        }
+    }
+}
